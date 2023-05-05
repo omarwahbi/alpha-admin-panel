a4936faab0e8214314d311f225cf4de20adb4b49
omarwahbi <99210606+omarwahbi@users.noreply.github.com>
2023-05-06 00:53:37 +0300

    second vercel deploy attempt

--- db-old.js
+++ db-old.js
@@ -1,7 +1,7 @@
-import mysql from "mysql";
-export const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Omarwahbi97",
-  database: "alpha-website",
-});
+// import mysql from "mysql";
+// export const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "Omarwahbi97",
+//   database: "alpha-website",
+// });
