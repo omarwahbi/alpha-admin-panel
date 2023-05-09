db738221e3c4018871f00444b6f80c74efb90eb2
omarwahbi <99210606+omarwahbi@users.noreply.github.com>
2023-05-10 01:35:20 +0300

    Added cors the last time after last time

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
+import cors from "cors";
 import authRoutes from "./routes/auth.js";
 import cookieParser from "cookie-parser";
 import testimonialsRoutes from "./routes/testimonials.js";
@@ -9,10 +10,13 @@ import categoriesRoutes from "./routes/categories.js";
 import logosRoutes from "./routes/logos.js";
 import contactUs from "./routes/contactUs.js";
 
-const cors = require("cors");
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
