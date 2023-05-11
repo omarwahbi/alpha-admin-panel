9837d5dc6c7e92927eaa884f55532693c4c89f54
omarwahbi <99210606+omarwahbi@users.noreply.github.com>
2023-05-12 01:26:58 +0300

    fixed cors again and again and again

--- index.js
+++ index.js
@@ -18,8 +18,9 @@ app.use(
   cors({
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: "*",
-    preflightContinue: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+    preflightContinue: false,
     optionsSuccessStatus: 204,
   })
 );
