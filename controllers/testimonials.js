6821ab10476fca91a8e594b111636d59937a0445
omarwahbi <99210606+omarwahbi@users.noreply.github.com>
2023-05-14 00:37:55 +0300

    added req.cookie in add testimonial res

--- controllers/testimonials.js
+++ controllers/testimonials.js
@@ -19,7 +19,7 @@ export const getTestimonial = (req, res) => {
 
 export const addTestimonial = (req, res) => {
   const token = req.cookies.access_token;
-  if (!token) return res.status(401).json("Not authenticated!");
+  if (!token) return res.status(401).json(req.cookies);
 
   jwt.verify(token, secret, { ignoreExpiration: false }, (err, userInfo) => {
     if (err) return res.status(403).json("Token is not valid!");
